3e0cdf139f1c532ee127e02ebd931f4419f85db2
joaopauloteles19 <58312019+joaopauloteles19@users.noreply.github.com>
2025-01-06 17:34:03 +0000

    mensagemmodificada

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(nome){
-    return <h1>Jamais pense que não é capaz, que tem capacita é DEUS</h1>
+    return <h1>Jamais pense que não é capaz, quem te capacita é DEUS</h1>
 }
 export default Home;
